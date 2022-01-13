3c22986027997c1a60618d831bea86a4e6160c06
Vikalp Malhotra <kimboho@gmail.com>
2022-01-13 14:02:08 -0500

    commit tree 0d7f0d3524039b9e409b2106c2c59adfe37ce97b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  //set state 
+  const [incart, setCart] = useState(false);
+
+
+
+  //create a click handler
+  function handleClick() {
+
+    setCart((incart) => !incart)
+
+  }
+
   return (
-    <li className="">
+    // set the className to 'in-cart' if incart is true and nothing if incart is false.  
+    <li className={incart ? "in-cart" : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+        onClick={handleClick}
+        //change the button class name to remove (if incart is true) or add (if incart is false)
+        //if incart is true add the text Remove from if its false add the text Add to
+        className={incart ? 'remove' : "add"}>{incart ? 'Remove From' : 'Add to'} Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setList] = useState('All');
+
+  function handleClick(event) {
+    setList(event.target.value)
+
+  }
+
+  // build an array from items and return the full array when selectedCategory = All,  if not filter the array for only items that equal the selectedCategory.
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === 'All') return true;
+
+    return item.category === selectedCategory;
+  });
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleClick}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
